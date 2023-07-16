3e515ce83e90994fdf608c77e264cc2ff9bdea04
Baby <89675173+Swag666baby@users.noreply.github.com>
2023-07-15 21:01:50 -0300

    Delete urlLongest.js

--- src/routes/urlLongest.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var router = (0, express_1.Router)();
-router.post("/longest", function (request, response) {
-});
-exports.default = router;
